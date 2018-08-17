af611f08f0365c6ab4c7eea69a12a1941b64b2dc
Jeffrey Thomas Piercy <mqduck@mqduck.net>
2018-08-16 23:12:31 -0700

    Initial commit

---
+++ app/src/main/java/org/subhipstercollective/dd5echaractersheet/MainActivity.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2018 Jeffrey Thomas Piercy
+ *
+ * This file is part of D&D 5e Character Sheet.
+ *
+ * D&D 5e Character Sheet is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * D&D 5e Character Sheet is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * D&D 5e Character Sheet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.subhipstercollective.dd5echaractersheet
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                message.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_dashboard -> {
+                message.setText(R.string.title_dashboard)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notifications -> {
+                message.setText(R.string.title_notifications)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
